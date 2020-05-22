3c7df3c183ea1b24fba3afe6a988228a12558cef
Ansel Zhang <ansel.zhang@ef.com>
2020-05-22 17:47:13 +0800

    update - conf

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/styleCond.ts',
   output: {
+    sourcemap: true,
     interop: true,
     dir: 'build',
     format: 'cjs'
